50d902805cf495fe68f4ad4c7d158562579b1b20
Paul Gathogo <pgathogo@gmail.com>
2017-04-14 12:52:28 +0300

    Added a GitWrapper class

--- git_editor.py
+++ git_editor.py
@@ -1,7 +1,36 @@
 import sys
+import os
+
 from PyQt4.QtGui import QApplication
 
+import git
+from git import Repo
+
 from help_editor import HelpEditor
+
+class GitWrapper(object):
+    def __init__(self, repo):
+        if not os.path.exists(repo):
+            print "Repo path does not exists!"
+            return
+        else:
+            try:
+                self.repo = Repo(repo)
+            except:
+                print "Not a valid git repo!"
+                return
+
+    def git_add(self):
+        pass
+    def git_commit(self):
+        pass
+    def git_clone(self):
+        pass
+    def get_modified_files(self):
+        mfiles = []
+        mfiles = self.repo.git.diff(name_only=True).split('\n')
+        return mfiles
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
